017667c293c6f31f3114cd3d3a48bf2f72523feb
OpsWorks user jwalsh <jwalsh@sunlight.localdomain>
2015-05-25 18:15:55 +0000

    Added user agent and download pause

--- input/download_bulk_sunlight_files.sh
+++ input/download_bulk_sunlight_files.sh
@@ -2,10 +2,11 @@
 
 eval $(cat ../default_profile | sed 's/^/export /')
 
-state_abbrevs=$(psql -t -c "SELECT abbreviation FROM input.state_metadata WHERE bills_identified IS NULL ORDER BY abbreviation;")
+state_abbrevs=$(psql -t -c "SELECT abbreviation FROM input.state_metadata WHERE bills_identified IS NULL AND abbreviation > 'l' ORDER BY abbreviation;")
 
 for i in $state_abbrevs; do
-	wget -O /mnt/data/sunlight/openstates_zipped_files/${i} http://static.openstates.org/downloads/2015-05-01-${i}-json.zip
+	wget -U "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36" -d -O /mnt/data/sunlight/openstates_zipped_files/${i} http://static.openstates.org/downloads/2015-05-01-${i}-json.zip
+	sleep 60
 done
 
 
