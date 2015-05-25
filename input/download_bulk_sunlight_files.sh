775d2635445f0004c120d475d55ccf231683af69
Joe Walsh <j.thomas.walsh@gmail.com>
2015-05-25 11:25:52 -0500

    fixed drake file to download state zips

--- input/download_bulk_sunlight_files.sh
+++ input/download_bulk_sunlight_files.sh
@@ -4,7 +4,7 @@
 state_abbrevs=$(psql -c "\COPY (SELECT abbreviation FROM input.state_metadata WHERE bills_identified IS NULL ORDER BY abbreviation LIMIT 1) TO STDOUT;")
 
 for element in $state_abbrevs; do
-	wget -O /mnt/data/sunlight/openstates_zipped_files/$(element) http://static.openstates.org/downloads/2015-05-01-$(element)-json.zip
+	wget -O /mnt/data/sunlight/openstates_zipped_files/$element http://static.openstates.org/downloads/2015-05-01-$element-json.zip
 done
 
 #echo $state_abbrevs
