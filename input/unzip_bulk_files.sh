14684a19af89ab5906055f8184045e719af53965
OpsWorks user jwalsh <jwalsh@sunlight.localdomain>
2015-05-26 05:26:27 +0000

    unzip files to directories with clean names (not .zip, etc)

--- input/unzip_bulk_files.sh
+++ input/unzip_bulk_files.sh
@@ -3,5 +3,6 @@
 filenames=$(ls /mnt/data/sunlight/openstates_zipped_files/)
 
 for i in $filenames; do
-	unzip $i -d /mnt/data/sunlight/openstates_unzipped/
+	dir_name=$(sed -E 's/201[0-9]-0[0-9]-0[0-9]-//g' ${i} | sed -E 's/-json.zip//g')
+	unzip /mnt/data/sunlight/openstates_zipped_files/${i} -d /mnt/data/sunlight/openstates_unzipped/${dir_name}
 done
