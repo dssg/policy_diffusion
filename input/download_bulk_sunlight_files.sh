44ea159590bb2680f9e25f4b006d3ed431369485
Joe Walsh <j.thomas.walsh@gmail.com>
2015-05-25 21:58:23 -0500

    Moved from wget loop to single curl statement

--- input/download_bulk_sunlight_files.sh
+++ input/download_bulk_sunlight_files.sh
@@ -4,10 +4,14 @@ eval $(cat ../default_profile | sed 's/^/export /')
 
 state_abbrevs=$(psql -t -c "SELECT abbreviation FROM input.state_metadata WHERE bills_identified IS NULL AND abbreviation > 'l' ORDER BY abbreviation;")
 
+user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36"
+month="05"  # the first day of this month is the last day of records to download
+
 for i in $state_abbrevs; do
-	wget -U "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36" -d -O /mnt/data/sunlight/openstates_zipped_files/${i} http://static.openstates.org/downloads/2015-05-01-${i}-json.zip
-	sleep 60
+	urls="$urls -O http://static.openstates.org/downloads/2015-${month}-01-${i}-json.zip"
 done
+
+curl -A '$user_agent' $urls
 
 
 # GRAB STATES TRACKED BY SUNLIGHT
