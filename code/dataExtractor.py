f11296d26293ccfa3c70e8e610824ec0df7dc56f
Julian Katz-Samuels <jkatzsam@umich.edu>
2015-06-03 11:41:57 -0400

    added html function

--- code/dataExtractor.py
+++ code/dataExtractor.py
@@ -1,7 +1,7 @@
 #Functions for extracting data
 
 from PyPDF2 import PdfFileReader
-
+from bs4 import BeautifulSoup
 
 ################################
 #pdf functions
@@ -20,6 +20,25 @@ def pdfToText(path):
 	output = ""
 	for page in PDF.pages:
 	     output = output + page.extractText()
+
+	return output
+
+################################
+#html functions
+
+test_url_path = "/Users/jkatzsamuels/Desktop/dssg/sunlight/test_code/test.html"
+
+def urlToText(path):
+	"""
+	Args:
+		path to html file
+
+	Returns:
+		a string of the html file
+	"""
+	with open(path,'r') as html:
+		soup = BeautifulSoup(html)
+	output = soup.get_text()
 
 	return output
 
