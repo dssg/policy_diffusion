69d27bd7f1b4ac357a84705b72e20bb4997951fa
Julian Katz-Samuels <jkatzsam@umich.edu>
2015-06-03 15:06:43 -0400

    added docToText function to dataExtractor

--- code/dataExtractor.py
+++ code/dataExtractor.py
@@ -2,6 +2,9 @@
 
 from PyPDF2 import PdfFileReader
 from bs4 import BeautifulSoup
+from docx import Document #python-docx pacakage
+import os
+import codecs
 
 ################################
 #pdf functions
@@ -26,8 +29,6 @@ def pdfToText(path):
 ################################
 #html functions
 
-test_url_path = "/Users/jkatzsamuels/Desktop/dssg/sunlight/test_code/test.html"
-
 def urlToText(path):
 	"""
 	Args:
@@ -36,10 +37,36 @@ def urlToText(path):
 	Returns:
 		a string of the html file
 	"""
-	with open(path,'r') as html:
+	with codecs.open(path, encoding='utf-8') as html:
 		soup = BeautifulSoup(html)
 	output = soup.get_text()
 
 	return output
+
+
+################################
+#doc functions
+
+def docToText(path):
+	"""
+	Turns docx file into a string.
+
+	Note: it concatenates paragraphs and does not 
+	distinguish between beginning and end of paragraphs
+
+	Args:
+		path to docx file
+
+	Returns:
+		a string of the docx file
+	"""
+	doc = Document(path)
+
+	output = ""
+	for paragraph in doc.paragraphs:
+		output = output + paragraph.text
+
+	return output
+
 
 
