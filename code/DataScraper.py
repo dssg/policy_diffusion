4f586555d50ffaeae217daea5746393b9ca8cb7e
mattburg <mattburg@Matthews-MacBook-Pro.local>
2015-06-23 13:31:51 -0500

    finished bill scraper and removed extraneous extractor files

--- code/DataScraper.py
+++ code/DataScraper.py
@@ -1,143 +1,167 @@
-import ujson
 import json
 import codecs
 import base64
-import urllib2
 import logging
-import time
 import re
 import os
 import sys
 import multiprocessing
+import utils
+import random
+import argparse
 import traceback
+
+try:
+    from os import scandir, walk
+except ImportError:
+    from scandir import scandir, walk
 
 BILL_SCRAPER_LOG = os.environ['POLICY_DIFFUSION'] + '/logs/bill_scraper.log'
 
 
-
-# scrapes all bills from file in /misc/bill_file_paths_random.tx
-def scrape_all_bills():
+# scrapes all bills from the input data path
+def scrape_all_bills(bill_data_path, num_workers):
     logging.basicConfig(filename=BILL_SCRAPER_LOG, level=logging.DEBUG)
 
-    filePaths = open("/mnt/data/sunlight/dssg/misc/bill_file_paths_random.txt").readlines()
+    bill_file_paths = []
+    for dirname, dirnames, filenames in walk(bill_data_path):
+        for filename in filenames:
+            bill_file_paths.append(os.path.join(dirname, filename))
 
-    pool = multiprocessing.Pool(20)
 
-    pool.map(scrape_bill_document_from_sunlight, filePaths)
+    scrape_bill_document_from_sunlight(bill_file_paths[0])
 
+    random.shuffle(bill_file_paths)
 
-#    for filePath in filePaths[0:100]:
-#        scrape_bill_document(filePath,outDirRootPath)
+    pool = multiprocessing.Pool(num_workers)
 
-# open individual json file and scrape bill document,
-# from the original source, usually state website
-def scrape_bill_document_from_original_source(filePath):
-    try:
-        filePath = filePath.strip()
+    print "fetch {0} urls from sunlight...".format(len(bill_file_paths))
+    pool.map(scrape_bill_document_from_sunlight, bill_file_paths)
 
-        outFilePath = "/".join(filePath.split("/")[7:])
-        outFilePath = re.sub("\s+", "_", outFilePath)
-        outDirRootPath = "/mnt/data/sunlight/dssg/scraped_bills"
-        outFileName = "{0}/{1}.json".format(outDirRootPath, outFilePath)
-
-        # check if file exists
-        if os.path.isfile(outFileName):
-            return
-
-        billFile = codecs.open(filePath, encoding="utf8").read()
-        billJson = ujson.decode(billFile)
-
-        urls = [x['url'] for x in billJson['versions']]
-        urls = [re.sub("\s", "%20", url) for url in urls]
-        for i, url in enumerate(urls):
-
-            time.sleep(1.0)
-            scrapeCount = 0
-            while scrapeCount <= 3:
-
-                try:
-                    url = billJson['versions'][-1]["url"]
-                    url = re.sub("\s", "%20", url)
-                    billDocument = urllib2.urlopen(url, timeout=10).read()
-                    billDocument = base64.b64encode(billDocument)
-                    billJson['versions'][i]['bill_document'] = billDocument
-                    break
-                except urllib2.URLError:
-                    billJson['versions'][i]['bill_document'] = None
-                    logging.error("file {0} ,version {1}, error: << {2} >>".format(filePath, i, "no link error"))
-                    break
-
-            if scrapeCount > 3:
-                billJson['versions'][i]['bill_document'] = None
-                logging.error("file: {0} , error: {1}, url:  {2}".format(filePath, "broke url error", url))
-                break
-
-        if not os.path.exists(os.path.dirname(outFileName)):
-            os.makedirs(os.path.dirname(outFileName))
-        with codecs.open(outFileName, "w", encoding="utf8") as f:
-            f.write(json.dumps(billJson))
-
-        logging.info("successfully scraped bill: {0}".format(outFilePath))
-
-        return
-
-    except Exception as e:
-        traceMessage = re.sub("\n+", "\t", traceback.format_exc())
-        traceMessage = re.sub("\s+", " ", traceMessage)
-        traceMessage = "<<{0}>>".format(traceMessage)
-        m = "Failed to obtain documents for {0}: {1}".format(filePath, traceMessage)
-        logging.error(m)
+    print "finished fetching urls..."
 
 
 # open individual json file and scrape bill document,
 # from the s3 server provided by sunlight foundation
-def scrape_bill_document_from_sunlight(filePath):
+def scrape_bill_document_from_sunlight(file_path):
     try:
-        filePath = filePath.strip()
+        file_path = file_path.strip()
 
-        outFilePath = "/".join(filePath.split("/")[7:])
-        outFilePath = re.sub("\s+", "_", outFilePath)
-        outDirRootPath = "/mnt/data/sunlight/dssg/scraped_bills"
-        outFileName = "{0}/{1}.json".format(outDirRootPath, outFilePath)
+        #define path to write file
+        out_file_path = "/".join(file_path.split("/")[6:])
+        out_file_path = re.sub("\s+", "_", out_file_path)
+        out_dir_root_path = "/mnt/data/sunlight/dssg/scraped_bills_new"
+        out_file_name = "{0}/{1}.json".format(out_dir_root_path, out_file_path)
 
-        billFile = codecs.open(filePath, encoding="utf8").read()
-        billJson = ujson.decode(billFile)
+        bill_json = json.loads(codecs.open(file_path, encoding="utf8").read())
 
-        baseUrl = "{0}/{1}".format("http://static.openstates.org/documents", filePath.split("/")[7])
-        urls = ["{0}/{1}".format(baseUrl, x['doc_id']) for x in billJson['versions']]
+        # filters documents that are resolutions
+        bill_text_count = [1 for x in bill_json['type'] if "bill" in x.lower()]
+        if sum(bill_text_count) < 1:
+            return
+
+
+        # filter versions to be only the first and last
+        try:
+            bill_json['versions'] = [bill_json['versions'][0], bill_json['versions'][-1]]
+        except IndexError:
+            return
+
+        base_url = "{0}/{1}".format("http://static.openstates.org/documents", bill_json['state'])
+        urls = ["{0}/{1}".format(base_url, x['doc_id']) for x in bill_json['versions']]
 
         for i, url in enumerate(urls):
 
-            try:
-                billDocument = urllib2.urlopen(url, timeout=10).read()
-                billDocument = base64.b64encode(billDocument)
-                billJson['versions'][i]['bill_document'] = billDocument
-            except urllib2.URLError:
-                billJson['versions'][i]['bill_document'] = None
-                logging.error("file {0} ,version {1}, error: << {2} >>".format(filePath, i, "link error"))
+            bill_document = utils.fetch_url(url)
 
-        if not os.path.exists(os.path.dirname(outFileName)):
-            os.makedirs(os.path.dirname(outFileName))
-        with codecs.open(outFileName, "w", encoding="utf8") as f:
-            f.write(json.dumps(billJson))
+            #hash bill using base64
+            if bill_document is not None:
+                bill_document = base64.b64encode(bill_document)
+            else:
+                logging.error("file {0}, url {1}, version {2}, error: << {3} >>".format(file_path, url, i, "link error"))
 
-        logging.info("successfully scraped bill: {0}".format(outFilePath))
+            bill_json['versions'][i]['bill_document'] = bill_document
 
-        return
+        if not os.path.exists(os.path.dirname(out_file_name)):
+            os.makedirs(os.path.dirname(out_file_name))
+        with codecs.open(out_file_name, "w", encoding="utf8") as f:
+            f.write(json.dumps(bill_json))
+
+        logging.info("successfully scraped bill: {0}".format(out_file_path))
 
     except Exception as e:
-        traceMessage = re.sub("\n+", "\t", traceback.format_exc())
-        traceMessage = re.sub("\s+", " ", traceMessage)
-        traceMessage = "<<{0}>>".format(traceMessage)
-        m = "Failed to obtain documents for {0}: {1}".format(filePath, traceMessage)
+        trace_message = re.sub("\n+", "\t", traceback.format_exc())
+        trace_message = re.sub("\s+", " ", trace_message)
+        trace_message = "<<{0}>>".format(trace_message)
+        m = "Failed to obtain documents for {0}: {1}".format(file_path, trace_message)
         logging.error(m)
+
+    return
+
+
+# scrapes bill document from original source link
+# this is a backup if s3 doesn't work
+def scrape_bill_document_from_original_source(filePath):
+    filePath = filePath.strip()
+
+    outFilePath = "/".join(filePath.split("/")[7:])
+    outFilePath = re.sub("\s+", "_", outFilePath)
+    outDirRootPath = "/mnt/data/sunlight/dssg/scraped_bills_new"
+    outFileName = "{0}/{1}.json".format(outDirRootPath, outFilePath)
+
+    billFile = codecs.open(filePath, encoding="utf8").read()
+    billJson = json.loads(billFile)
+
+    # filters documents that are resolutions
+    bill_text_count = [1 for x in billJson['type'] if "bill" in x.lower()]
+    if sum(bill_text_count) < 1:
+        return
+
+    # filter versions to be only the first and last
+    billJson['versions'] = [billJson['versions'][0], billJson['versions'][-1]]
+
+    urls = [x['url'] for x in billJson['versions']]
+
+    for i, url in enumerate(urls):
+
+        billDocument = utils.fetch_url(url)
+
+        if billDocument is not None:
+            billDocument = base64.b64encode(billDocument)
+        else:
+            logging.error("file {0}, url {1}, version {2}, error: << {3} >>".format(filePath, url, i, "link error"))
+
+        billJson['versions'][i]['bill_document'] = billDocument
+
+    if not os.path.exists(os.path.dirname(outFileName)):
+        os.makedirs(os.path.dirname(outFileName))
+    with codecs.open(outFileName, "w", encoding="utf8") as f:
+        f.write(json.dumps(billJson))
+
+    logging.info("successfully scraped bill: {0}".format(outFilePath))
+
+    return
+
+
 
 
 def main():
-    command = sys.argv[1]
 
-    if command == "scrape_bills":
-        scrape_all_bills()
+    parser = argparse.ArgumentParser(description='module that contains functions to scrape legislative data\ '
+                                                 ' from sunlight foundation and various'
+                                                 'lobbying organizations')
+    parser.add_argument('command', help='command to run, options are: \n scrape_bills_from_sunlight')
+    parser.add_argument('--data_path', dest='data_path', help="file path of data to be indexed ")
+    parser.add_argument('--num_workers', dest='num_workers',default = 10,
+                        type = int, help="file path of data to be indexed ")
+
+    args = parser.parse_args()
+
+    if args.command == "scrape_bills_from_sunlight":
+        scrape_all_bills(args.data_path,args.num_workers)
+    else:
+        print("command not recognized, use -h flag to see list available commands")
+
 
 
 if __name__ == "__main__":
--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,11 @@
 import ujson
 import base64
+import urllib2
+import socket
+from ftplib import FTP, error_perm
+import re
+from StringIO import StringIO
+import time
 
 #creates a searalized json object for bill sources
 def bill_source_to_json(url,source,date):
@@ -11,3 +17,40 @@ def bill_source_to_json(url,source,date):
     return ujson.encode(jsonObj)
 
 
+#wrapper for urllib2.urlopen that catches URLERROR and socket error
+def fetch_url(url):
+
+    #fetch ftp file
+    if 'ftp://' in url:
+        try:
+            domain_pattern = re.compile("/[A-Za-z0-9\.]+")
+            domain_name = domain_pattern.search(url).group(0)[1:]
+            ftp = FTP(domain_name,timeout=10)
+            ftp.login()
+            file_name = "/".join(url.split("/")[3:])
+
+            r = StringIO()
+            ftp.retrbinary('RETR {0}'.format(file_name), r.write)
+            document = r.getvalue()
+            time.sleep(1)
+
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            document = None
+
+
+        return document
+
+    #fetch http file
+    else:
+
+        try:
+            req  = urllib2.urlopen(url,timeout=10)
+            document = req.read()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            document = None
+
+        return document
