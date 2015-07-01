548bd30e155e0bdbd19f6c2f3be5dcef10e39b97
Julian Katz-Samuels <jkatzsam@umich.edu>
2015-07-01 11:37:37 -0400

    removed old scraper files and added function to scrapers for scraping misc bills

--- code/scrapers.py
+++ code/scrapers.py
@@ -277,6 +277,27 @@ def scrape_ALICE_legislation():
 
     print str(badCount) + " did not work"
 
+def scrape_misc_legislation():
+        # Access list of clean urls
+    with open('/Users/jkatzsamuels/Desktop/dssg/sunlight/policy_diffusion/data/model_legislation_urls/clean_urls.txt','r') as f:
+        links = f.read().splitlines()
+
+    badCount = 0
+    goodCount = 0
+    with open('misc_bills.json', 'w') as jsonfile:
+        for link in links:
+            try:
+                source = urllib2.urlopen(link).read()
+                Jsonbill = bill_source_to_json(link, source, None)
+                jsonfile.write("{0}\n".format(Jsonbill))
+                goodCount += 1
+                print goodCount
+            except:
+                badCount += 1
+
+    print str(badCount) + " did not work"
+    print str(goodCount) + " worked"
+
 
 
 def main():
@@ -299,6 +320,8 @@ def main():
         scrape_CSG_model_legislation()
     elif args.command == "scrape_ALICE_legislation":
         scrape_ALICE_legislation()
+    elif args.command =="scrape_misc_legislation":
+        scrape_misc_legislation()
     else:
         print("command not recognized, use -h flag to see list available commands")
 
