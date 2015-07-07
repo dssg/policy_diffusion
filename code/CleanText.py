ff1af62e4bbc987b1dbd0d766750ed6e560bba6c
Julian Katz-Samuels <jkatzsam@umich.edu>
2015-07-07 09:39:46 -0400

    made into functions

--- code/CleanText.py
+++ code/CleanText.py
@@ -8,6 +8,13 @@ import string
 import urllib2
 from tika import parser
 from collections import defaultdict
+from elasticsearch import Elasticsearch
+from pprint import pprint
+import nltk
+
+#custom modules
+from database import ElasticConnection
+
 
 
 #TODO: make the below code into functions that work well with elastic search interface
@@ -42,15 +49,8 @@ nonMatches = ['http://www.alec.org/model-legislation/21st-century-commercial-nex
                 'http://www.alec.org/model-legislation/alternative-certification-act/',
                 'http://www.alec.org/model-legislation/anti-phishing-act/']
 
-test = nonMatches[2]
-doc = urllib2.urlopen(test).read()
-test_text = parser.from_buffer(doc)['content']
-
-##line-by-line attempt
-test_list = string.split(test_text,'\n')
-
 ##########################
-#Test Functions
+#Predicate Functions for Cleaning
 def isInt(s):
     '''
     Tests whether n can be turned into an integer
@@ -82,14 +82,14 @@ def isLong(s, n):
             return True
     return False
 
-def isTabs(s):
-    '''
-    checks if line consists only in tabs
-    '''
-    if re.search(r'\t+',s) != None:
-        return True
-    else:
-        return False
+# def isTabs(s):
+#     '''
+#     checks if line consists only in tabs
+#     '''
+#     if re.search(r'\t+',s) != None:
+#         return True
+#     else:
+#         return False
 
 def isSpace(s):
     '''
@@ -97,82 +97,104 @@ def isSpace(s):
     '''
     return s.split() == []
 
-##Loop
-# keep =[]
-# prev_newline = 0
-#Cleaning involves:
-#1. delete lines with only a number on them and 
-#2. condense multiple empty lines into one empty line
-#3. remove name
-# bill_name = test[u'_source'][u'bill_id']
-# for i in range(len(test_list)):
-# 	line = test_list[i]
-# 	try: 
-# 		int(line)
-# 	except:
-# 		if line != '':
-# 			prev_newline = 0
-# 			keep.append(line)
-# 		elif line == '' and prev_newline == 1:
-# 			continue
-# 		elif line == '' and prev_newline == 0:
-# 			prev_newline = 1
-# 			keep.append(line)
 
-# final_text = string.join(keep, '\n')
-
-
-#replace funky symbols 
-for i in range(len(test_list)):
-    test_list[i] = test_list[i].replace(u'\xa0', u' ')
-
-# bill_name = test[u'_source'][u'bill_id']
-bill_name = 'HB 2011'
-keep =[]
-prev_newline = 0
-for i in range(len(test_list)):
-    line = test_list[i]
-    if isLong(line,50) or isTabs(line) or isInt(line):
-        continue
-    if prev_newline == 1 and isSpace(line):
-        continue
-    elif prev_newline == 0 and isSpace(line):
-          prev_newline = 1
-          keep.append(line)
-    else:
-        if isHeaderFooter(line, bill_name):
-            continue
-        else:
-            keep.append(line)
-            prev_newline = 0
-
-
-final_text = string.join(keep, '\n')
-
-
-##separate text into sections
-#return as dictionary with section as key
-#NOTE: that it forgets everything before first section
 def isNewSection(s):
     '''
     Determines whether s is a new section.
     '''
-    if re.match(r'^Section \d', s) != None:
+    if re.match(r'^section \d', s) != None:
         return True
     else:
         return False
 
-sections = defaultdict(list)
-seen_section = 0 #have we seen a section yet
-for line in keep:
-    if isNewSection(line):
-        seen_section = 1
-        section = line
-        sections[section].append(line)
-    elif seen_section == 0:
-        continue
-    else:
-        sections[section].append(line)
+##########################
+#Main clean function
+
+def clean_text(text, bill_name, lower = 1):
+    '''
+    text:
+        text: string corresponding to text of bill
+        bill_name: string corresponding to name of the bill
+
+    returns:
+        tuple with one field that is string that is cleaned up text 
+        and the other field is a dictionary corresponding to sections
+    decription:
+        clean text and return dictionary with sections
+        note that the dictionary of sections only contains material after 
+        the first section
+    '''
+    #parse by line
+    text_list = string.split(text,'\n')
+
+    if lower == 1:
+        text_list = [text.lower() for text in text_list]
+
+    #replace funky symbols 
+    for i in range(len(text_list)):
+        text_list[i] = text_list[i].replace(u'\xa0', u' ')
+
+    keep =[]
+    prev_newline = 0
+    for i in range(len(text_list)):
+        line = text_list[i]
+        if isLong(line,50) or isInt(line):
+            continue
+        if prev_newline == 1 and isSpace(line):
+            continue
+        elif prev_newline == 0 and isSpace(line):
+              prev_newline = 1
+              keep.append(line)
+        else:
+            if isHeaderFooter(line, bill_name):
+                continue
+            else:
+                keep.append(line)
+                prev_newline = 0
+
+
+    sections = defaultdict(list)
+    seen_section = 0 #have we seen a section yet
+    for line in keep:
+        if isNewSection(line):
+            seen_section = 1
+            section = line
+            sections[section].append(line)
+        elif seen_section == 0:
+            continue
+        else:
+            sections[section].append(line)
+
+    return (string.join(keep, '\n'), sections)
+
+
+
+def main():
+    es = ElasticConnection(host = "54.212.36.132")
+
+    bills = es.query_state_bills('test')
+
+    for bill in bills:
+        bill_name = bill['title']
+        (cleaned, sections) = clean_text(bill['doc_text'], bill_name)
+
+        print 'original text: ' + '\n'
+        pprint(bill['doc_text'])
+        print '\n'
+        print 'cleaned text: ' + '\n'
+        pprint(cleaned)
+        print '\n'
+        print 'sections: ' + '\n'
+        pprint(sections)
+        # print '\n'
+        # print 'list: ' + '\n'  
+        # pprint(nltk.word_tokenize(cleaned))
+        # pprint(cleaned.split())
+        raw_input("Press Enter to continue...")
+
+
+if __name__ == '__main__':
+    main()
 
 
 
