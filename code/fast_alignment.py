6588cabfa14d46348a19ff5cc7f9176bf116173a
Julian Katz-Samuels <jkatzsam@umich.edu>
2015-07-11 21:54:34 -0400

    added backtrace function and testing function; still need to check speed of backtrace

--- code/fast_alignment.py
+++ code/fast_alignment.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numba import jit
+from alignmentFunctions import seqToAlign
 
 '''
 class LocalSequenceAligner(SequenceAligner):
@@ -74,7 +75,7 @@ class LocalSequenceAligner(SequenceAligner):
 '''
 
 @jit
-def computeAlignmentMatrix(left, right,match_score,gap_score,mismatch_score):
+def computeAlignmentMatrix(left, right,match_score,mismatch_score, gap_score):
     m = len(left) + 1
     n = len(right) + 1
     score_matrix = np.zeros((m, n), int)
@@ -100,3 +101,103 @@ def computeAlignmentMatrix(left, right,match_score,gap_score,mismatch_score):
     
     return score_matrix,pointer_matrix
 
+@jit
+def backtrace(left, right, score_matrix, pointer_matrix, gap = '-'):
+    '''
+    returns
+        left_alignment
+        right_alignment
+    '''
+    i,j = np.unravel_index(score_matrix.argmax(), score_matrix.shape)
+    decision = pointer_matrix[i,j]
+
+    left_alignment = []
+    right_alignment = []
+    while decision != 0 and i > 0 and j > 0:
+        if decision == 1: #do not insert space
+            i -= 1
+            j -= 1
+            left_alignment.append(left[i])
+            right_alignment.append(right[j])
+        elif decision == 2: #insert space in right text
+            j -= 1
+            right_alignment.append(right[i])
+            left_alignment.append(gap)
+        elif decision == 3: #insert space in left text
+            i -= 1
+            left_alignment.append(left[i])
+            right_alignment.append(gap)
+
+        #update decision
+        decision = pointer_matrix[i,j]
+
+        # print 'decision: ' + str(decision)
+        # print 'i: ' + str(i) + ' j: ' + str(j)
+
+    return left_alignment, right_alignment
+
+
+def alignment_score(l,r,match_score,mismatch_score, gap_score):
+    score = 0
+    for i in range(len(l)):
+        if l[i] == r[i]:
+            score += match_score
+        elif l[i] == '-' or r[i] == '-':
+            score += gap_score
+        else:
+            score += mismatch_score
+
+    return score
+
+
+
+
+def test_alignment(t1,t2):
+    s,p=computeAlignmentMatrix(t1,t2,3,-1,-2)
+
+    score = s.max()
+
+    l,r = backtrace(t1,t2,s,p)
+
+    #find score of recovered alignment
+    align_score = alignment_score(l,r,3,-1,-2)
+
+    #run package algorithm
+    alignments = seqToAlign(t1,t2, 3, -1, -2)
+
+    print 'dp_alg_score: ' + str(score)
+    print 'alignment_score: ' + str(align_score)
+    print 'package_score: ' + str(alignments[0][0])
+
+
+    # l_true, r_true = alignments[0][1:]
+
+    # if len(l) != len(l_true) or len(r) != len(r_true):
+    #     print "Not the same alignment"
+    #     print 'length of l: ' + str(len(l))
+    #     print 'length of true l: ' + str(len(l_true))
+    #     print 'length of r: ' + str(len(r))
+    #     print 'length of true r: ' + str(len(r_true))
+
+    # for i in range(len(l)):
+    #     if l[i] != l_true[i]:
+    #         print "Not the same alignment"
+    #         print "left alignment difrence: " + str(l[i]) + ',' + str(l_true[i])
+    #         print '\n'
+    #         break
+
+    # for i in range(len(r)):
+    #     if r[i] != r_true[i]:
+    #         print "Not the same alignment"
+    #         print "right alignment difrence: " + str(r[i]) + ',' + str(r_true[i])
+    #         print 'index of failure: ' + str(i)
+    #         break
+
+t1 = ['a']*100
+
+t2 = ['b']*50 + ['a','a','b']*50
+
+s1 = [1]*100
+s2 = [2]*50 + [1,1,2]*50
+
+test_alignment(t1,t2)
